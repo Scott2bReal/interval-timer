7b7c1fc91fb082733d08b2512f74aa1b5da2e2f7
Scott Hoecker <hoeckers@gmail.com>
2025-01-26 11:19:49 -0600

    Allow for passing config to Timer component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,14 @@
+import { ComponentProps } from 'solid-js'
 import { TimerProvider } from '../context/TimerProvider'
 import { PhaseDisplay } from './PhaseDisplay'
 import { TimerConfig } from './TimerConfig'
 import { TimerControls } from './TimerControls'
 
-const Timer = () => {
+const Timer = (props: {
+  initialConfig?: ComponentProps<typeof TimerProvider>['initialConfig']
+}) => {
   return (
-    <TimerProvider>
+    <TimerProvider initialConfig={props.initialConfig}>
       <div class="mx-auto max-w-md rounded bg-stone-900 p-6 text-center text-stone-100 shadow-lg">
         <PhaseDisplay />
         <TimerControls />
